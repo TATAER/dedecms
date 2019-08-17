a9b3ff1bd669ab7833a6436f2dc8926927fde8f1
郭德法 <guodefa@xianlife.com>
2019-08-17 21:43:43 +0800

    fix

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = '47.106.72.245';
+$cfg_dbname = 'dede';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'uChB;r6rdmo-!';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
